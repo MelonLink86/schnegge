9850657e7dbe745d39790f8f7f53943efd4be531
MelonLink86 <melonfire44@gmail.com>
2022-05-22 01:17:25 +0200

    bastard

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ let food;
 let w;
 let h;
 let scooter;
-let bad;
 let drei;
 let benzin;
+let lol;
 let schlecht;
+let sans;
 let started = false;
 
 //function rassissmus(){
@@ -42,6 +43,7 @@ function foodLocation() {
   // }
   //}
 
+
 function keyPressed() {
   if (!started){
     benzin.play();
@@ -67,8 +69,9 @@ function keyPressed() {
 
 function preload() {
   scooter = loadImage('assets/scooter.jpg');
-  bad = loadImage('assets/bad.jpg');
   drei = loadImage('assets/drei.gif');
+  lol = loadImage('assets/lol.gif');
+  sans = loadImage('assets/sans.gif');
   benzin = loadSound('assets/benzin.mp3');
   schlecht = loadSound('assets/schlecht.mp3');
 }
@@ -81,26 +84,40 @@ function draw() {
 
   if (snake.endGame()) {
     benzin.stop();
-    schlecht.play();
-
+    if (!schlecht.isPlaying()){
+     schlecht.play();
+    }
     print('END GAME');
     background(255, 0, 0);
-      image(scooter, 0, 0, width/rez, height/rez)
-
+    image(scooter, 0, 0, width/rez, height/rez);
+      
     colorMode(HSB, 100);
     fill(frameCount*5%100, 100, 100);
     textSize(0.8);
-    textStyle(BOLD)
+    textStyle(BOLD);
     textAlign(CENTER);
     text('du hurensohn spiel spiele die du kannst du bastard', width/2/ rez, height/2/rez-1);
     text('so ein schwinepriester: ' + ((width/rez*height/rez)-snake.len), width/2/rez, height/2/rez+2.5);
-     
-  }
+    
+    translate(width/2/rez-5, height/2/rez-6);
+    rotate(frameCount/20%(2*PI));
+        imageMode(CENTER);
+        image(lol, 0, 0, 5, 5);   
+
+    translate(width/2/rez+5, height/2/rez-6);
+        rotate(-(frameCount/20%(2*PI)));
+        imageMode(CENTER);
+        image(sans, 0, 0, 5, 5);
+        imageMode(CORNER);
+        translate(0, 0);
+        rotate(0);    
+      }
+
   else {
     background(200);
     image(drei, 0, 0, width/rez, height/rez);
   
-    if (frameCount%6 == 0){
+    if (frameCount%(6-snake.len/4) < 1){
       if (snake.eat(food)) {
         foodLocation();
       }
